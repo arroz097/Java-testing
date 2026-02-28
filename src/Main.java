799700252f1562f299a596391ee7885f6deeff8e
arroz097 <luizgabrielmatera@hotmail.com>
2026-02-28 19:15:28 -0300

    ButtonCreator and Cat integration, buttons for each cat. Made some testing code inside main as a separated function

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.Random;
 
 public class Main {
 
+    private static final Utility utility = new Utility();
+    private static final Random rng = new Random();
+
     // nao é necessario sempre ter main, mas normalmente é onde o codigo principal vai rodar
     public static void main(String[] args) {
 
@@ -9,28 +12,55 @@ public class Main {
         // var ajuda a nao ter que repetir a tipagem de classes ao instanciar com new
         var jorge = new Cat("jorge");
         var eduardo = new Cat("eduardo");
-        var rng = new Random();
-        var utility = new Utility();
+        //var rng = new Random();
+        //var utility = new Utility();
         // var button = new ButtonTest();
 
         // se caso .isMoving nao fosse private
         // button.isMoving = false;
 
-        int valorAleatorio;
-        int valorDesignado;
-        int delay = 250;
-
+        // organizar frames u mem cima do outro
         var jorgeButton = new ButtonCreator("cat  " + jorge.name, "ROAR!", 200, 150, 100, 30);
         var eduardoButton = new ButtonCreator("cat " + eduardo.name, "ROAR!", 200, 150, 100, 30);
 
         var jorgeBed = new ButtonCreator(jorge.name + "'s bed", "sleep!", 200, 150, 100, 30);
         var eduardoBed = new ButtonCreator(eduardo.name + "'s bed", "sleep!", 200, 150, 100, 30);
 
+        var loopButton = new ButtonCreator("loop ft. rng", "do the loop!");
+
+        loopButton.frame.setLocation(loopButton.frame.getX(), 200);
+
+        // nao é a melhor das formas para posicionar um gui
+        // já que não posiciona de acordo com a dimensão da tela, e sim com valores puros absolutos
+        jorgeButton.frame.setLocation(jorgeButton.frame.getX() + 200, jorgeButton.frame.getY());
+        jorgeBed.frame.setLocation(jorgeButton.frame.getX(), jorgeButton.frame.getY() + 150);
+
+        // percebi que se spammar os botões as funções do OOP gato executam varias vezes, como se estivesse nuam fila
+        // java aparentemente coloca os cliques em uma fila de execução
+        // da pra desativar botão no clique ou criar um sistema de debounce
+        eduardoButton.frame.setLocation(eduardoButton.frame.getX() - 200, eduardoButton.frame.getY());
+        eduardoBed.frame.setLocation(eduardoButton.frame.getX(), eduardoButton.frame.getY() + 150);
+
         jorgeButton.button.addActionListener(e -> jorge.roar());
         eduardoButton.button.addActionListener(e -> eduardo.roar());
 
         jorgeBed.button.addActionListener(e -> jorge.sleep());
         eduardoBed.button.addActionListener(e -> eduardo.sleep());
+
+        loopButton.button.addActionListener(e -> {
+            loopButton.button.setEnabled(false);
+
+            // por ser na mesma thread, o loop vai impedir os outros botões de funcionarem até terminar
+            loopValue();
+        });
+
+    }
+
+    public static void loopValue() {
+
+        int valorAleatorio;
+        int valorDesignado;
+        int delay = 250;
 
         valorDesignado = rng.nextInt(60);
 
@@ -61,7 +91,7 @@ public class Main {
         System.out.println();
 
         System.out.println("loop encontrou valor aleatorio (" + valorDesignado + ") designado.");
-       // write(" loop encontrou valor aleatorio " + valorDesignado + " designado");
+        // write(" loop encontrou valor aleatorio " + valorDesignado + " designado");
 
         String phrase = writeReturn("i am a phrase wassup!");
 
